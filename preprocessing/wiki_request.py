e0947dbc97693c50dc6bb7b2df9013a16118c2ad
naelopode <nael.dillenbourg@epfl.ch>
2023-11-16 20:47:51 +0100

    remove unused function from wiki_request.py

--- preprocessing/wiki_request.py
+++ preprocessing/wiki_request.py
@@ -79,63 +79,6 @@ def get_IMDB_ID(wikiID):
     IMDB_ID = movie_item.get_json()['claims']['P345'][0]['mainsnak']['datavalue']['value']
     return IMDB_ID
 
-def get_gender_text(QID):
-    dictory = {
-        'male' : 'Q6581097',
-        'female' : 'Q6581072',
-        'intersex' : 'Q1097630',
-        'trans women' : 'Q1052281',
-        'trans men' : 'Q2449503',
-        'non-binary' : 'Q48270',
-        'fa\'afafine' : 'Q1399232',
-        'mahu' : 'Q3277905',
-        'kathoey' : 'Q746411',
-        'fakaleiti' : 'Q350374',
-        'hijra' : 'Q660882',
-        'two-spirit' : 'Q301702',
-        'transmasculine' : 'Q27679766',
-        'transfeminine' : 'Q27679684',
-        'muxe salu' : 'Q3177577',
-        'agender' : 'Q505371',
-        'genderqueer' : 'Q12964198',
-        'genderfluid' : 'Q18116794',
-        'neutrois' : 'Q1289754',
-        'eunuch' : 'Q179294',
-        'pangender' : 'Q7130936',
-        'neutral sex' : 'Q52261234',
-        'hermaphrodite' : 'Q16674976',
-        'cisgender woman' : 'Q15145779',
-        'cisgender man' : 'Q15145778',
-        'third gender' : 'Q48279',
-        'X-gender' : 'Q96000630',
-        'demiboy' : 'Q93954933',
-        'demigirl' : 'Q93955709',
-        'bigender' : 'Q859614',
-        'transgender' : 'Q189125',
-        'travesti' : 'Q17148251',
-        'akava\'ine' : 'Q4700377',
-        'assigned female at birth' : 'Q56315990',
-        'assigned male at birth' : 'Q25388691',
-        'androgyne' : 'Q97595519',
-        'yinyang ren' : 'Q8053770',
-        'boi' : 'Q99519347',
-        'intersex person' : 'Q104717073',
-        'gynandromorphism' : 'Q430711',
-        'takatapui' : 'Q7677449',
-        'undisclosed' : 'Q113124952',
-        'fakafifine' : 'Q112597587',
-        'androgynos' : 'Q4759445',
-        'intersex man' : 'Q121307094',
-        'intersex woman' : 'Q121307100',
-        'demimasc' : 'Q121368243'
-    }
-    inv_map = {v: k for k, v in dictory.items()}
-    try:
-        value = inv_map[QID]
-        return value
-    except KeyError:
-        print("No gender found gender id")
-
 def get_gender_id(QID):
     if QID == None:
         print('got none')
